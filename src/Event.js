32811fe99acfa9aae429bc9affdfc9fb8a5c65c4
wentdavid <davidwent@me.com>
2023-01-27 15:52:32 +0100

    add Event

---
+++ src/Event.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+
+    state = { collapsed: true };
+    toggleDetails = () => {
+        this.setState((prevState) => ({ collapsed: !prevState.collapsed }));
+    };
+
+  render() {
+    const { event } = this.props;
+    const { collapsed } = this.state;
+
+    return (
+      <div className="event">
+        <h1 className="summary">{event.summary}</h1>
+        <p className="event-start">{new Date(event.start.dateTime).toString()}</p>
+        <p className="event-location">{`@${event.summary} | ${event.location}`}</p>
+
+        {!collapsed && (
+          <div className="event-details">
+            <h2 className="about">About Event:</h2>
+            <a
+              className="link"
+              href={event.htmlLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              See more details
+            </a>
+            <p className="description">{event.description}</p>
+          </div>
+        )}
+        <button className="details-button" onClick={() => this.toggleDetails()}>
+          {collapsed ? "Show" : "Hide "} Details
+        </button>
+      </div>
+    );
+    }
+}
+
+export default Event;
