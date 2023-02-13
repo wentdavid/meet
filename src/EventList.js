78879496c0e88b04a5ffe7148abde20bf5e6a124
wentdavid <davidwent@me.com>
2023-02-13 16:20:17 +0100

    WarningAlert when user is offline

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Event from './Event';
+import {WarningAlert} from './Alert';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
+    const online = navigator.onLine;
     console.log("This events", events);
     return (
       <ul className="EventList">
+        {!online && <WarningAlert text="You are currently offline. Events may not be up to date." />}
         {events.map((event) => (
           <li key={event.id}>
             <Event event={event} />
